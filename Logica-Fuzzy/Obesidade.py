1421777f4780de9cba4d5905f62798232e80bc31
PATRICIA HELENA S MEDEIROS <unifpmedeiros>
2023-04-11 20:40:57 -0300

    add img

--- Logica-Fuzzy/Obesidade.py
+++ Logica-Fuzzy/Obesidade.py
@@ -10,11 +10,15 @@ calorias = ctrl.Antecedent(np.arange(0, 9, 1), 'calorias')
 peso = ctrl.Consequent(np.arange(0, 11, 1), 'peso')
 
 # automf -> Atribuição de categorias automaticamente
-calorias.automf(names=['pouco','razoável','bastante'],)
+calorias['pouco'] = fuzz.trapmf(calorias.universe, [-1,0,2,4])
+calorias['razoavel'] = fuzz.trapmf(calorias.universe,[ 2,4,6,8])
+calorias['bastante'] = fuzz.trapmf(calorias.universe, [4,6,8,9])
+
+#calorias.automf(names=['pouco','razoável','bastante'])
 
 # atribuicao sem o automf
-peso['peso leve'] = fuzz.gaussmf(peso.universe, 1,6)
-peso['peso médio'] = fuzz.gaussmf(peso.universe, 4, 10)
+peso['peso pena'] = fuzz.gaussmf(peso.universe, 1,6)
+peso['peso medio'] = fuzz.gaussmf(peso.universe, 4, 10)
 peso['peso pesado'] = fuzz.gaussmf(peso.universe, 8,11)
 
 
@@ -25,8 +29,8 @@ peso.view()
 
 
 #Criando as regras
-regra_1 = ctrl.Rule(calorias['pouco'], peso['peso leve'])
-regra_2 = ctrl.Rule(calorias['razoável'], peso['peso médio'])
+regra_1 = ctrl.Rule(calorias['pouco'], peso['peso pena'])
+regra_2 = ctrl.Rule(calorias['razoavel'], peso['peso medio'])
 regra_3 = ctrl.Rule(calorias['bastante'], peso['peso pesado'])
 
 controlador = ctrl.ControlSystem([regra_1, regra_2, regra_3])
@@ -43,12 +47,10 @@ valorpeso = Calculopeso.output['peso']
 
 print("\ncalorias %d \nServiço %d \npeso de %5.2f" %(
         notacalorias,
-        notaServico,
         valorpeso))
 
 
 calorias.view(sim=Calculopeso)
-servico.view(sim=Calculopeso)
 peso.view(sim=Calculopeso)
 
 plt.show()
