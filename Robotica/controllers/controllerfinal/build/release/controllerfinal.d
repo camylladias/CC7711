e41cc5dd1736fb9a6718d9ba35aec09028847dfb
EveSantana <87048983+EveSantana@users.noreply.github.com>
2023-05-30 20:10:04 -0300

    Versão final do projeto de IA

---
+++ Robotica/controllers/controllerfinal/build/release/controllerfinal.d
@@ -0,0 +1,11 @@
+build/release/controllerfinal.o: controllerfinal.c \
+ C:\Users\Eve\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\Eve\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\Eve\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\Eve\AppData\Local\Programs\Webots/include/controller/c/webots/device.h \
+ C:\Users\Eve\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Users\Eve\AppData\Local\Programs\Webots/include/controller/c/webots/led.h \
+ C:\Users\Eve\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\Eve\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\Eve\AppData\Local\Programs\Webots/include/controller/c/webots/supervisor.h \
+ C:\Users\Eve\AppData\Local\Programs\Webots/include/controller/c/webots/contact_point.h
---
+++ Robotica/controllers/controllerfinal/controllerfinal.c
@@ -0,0 +1,273 @@
+
+/*
+ * Copyright 1996-2021 Cyberbotics Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Description:  Default controller of the e-puck robot
+ */
+
+/* include headers */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <webots/robot.h>
+#include <stdio.h>
+#include <webots/device.h>
+#include <webots/distance_sensor.h>
+#include <webots/led.h>
+#include <webots/motor.h>
+#include <webots/nodes.h>
+#include <webots/robot.h>
+#include <webots/supervisor.h>
+#include <math.h>
+
+
+/* Device stuff */
+#define DISTANCE_SENSORS_NUMBER 8
+static WbDeviceTag distance_sensors[DISTANCE_SENSORS_NUMBER];
+static double distance_sensors_values[DISTANCE_SENSORS_NUMBER];
+static const char *distance_sensors_names[DISTANCE_SENSORS_NUMBER] = {"ps0", "ps1", "ps2", "ps3", "ps4", "ps5", "ps6", "ps7"};
+
+#define GROUND_SENSORS_NUMBER 3
+static WbDeviceTag ground_sensors[GROUND_SENSORS_NUMBER];
+static double ground_sensors_values[GROUND_SENSORS_NUMBER] = {0.0, 0.0, 0.0};
+static const char *ground_sensors_names[GROUND_SENSORS_NUMBER] = {"gs0", "gs1", "gs2"};
+
+#define LEDS_NUMBER 10
+static WbDeviceTag leds[LEDS_NUMBER];
+static bool leds_values[LEDS_NUMBER];
+static const char *leds_names[LEDS_NUMBER] = {"led0", "led1", "led2", "led3", "led4", "led5", "led6", "led7", "led8", "led9"};
+
+static WbDeviceTag left_motor, right_motor;
+
+#define LEFT 0
+#define RIGHT 1
+#define MAX_SPEED 6.28
+static double speeds[2];
+
+static bool is_pushing_box = false;
+static double initial_box_distance = 0.0;
+
+/* Breitenberg stuff */
+static double weights[DISTANCE_SENSORS_NUMBER][2] = {{-1.3, -1.0}, {-1.3, -1.0}, {-0.5, 0.5}, {0.0, 0.0},
+                                                     {0.0, 0.0},   {0.05, -0.5}, {-0.75, 0},  {-0.75, 0}};
+static double offsets[2] = {0.5 * MAX_SPEED, 0.5 * MAX_SPEED};
+
+static int get_time_step() {
+  static int time_step = -1;
+  if (time_step == -1)
+    time_step = (int)wb_robot_get_basic_time_step();
+  return time_step;
+}
+
+static void step() {
+  if (wb_robot_step(get_time_step()) == -1) {
+    wb_robot_cleanup();
+    exit(EXIT_SUCCESS);
+  }
+}
+
+static void passive_wait(double sec) {
+  double start_time = wb_robot_get_time();
+  do {
+    step();
+  } while (start_time + sec > wb_robot_get_time());
+}
+
+static void init_devices() {
+  int i;
+  for (i = 0; i < DISTANCE_SENSORS_NUMBER; i++) {
+    distance_sensors[i] = wb_robot_get_device(distance_sensors_names[i]);
+    wb_distance_sensor_enable(distance_sensors[i], get_time_step());
+  }
+  for (i = 0; i < LEDS_NUMBER; i++)
+    leds[i] = wb_robot_get_device(leds_names[i]);
+
+  // silently initialize the ground sensors if they exists
+  for (i = 0; i < GROUND_SENSORS_NUMBER; i++)
+    ground_sensors[i] = (WbDeviceTag)0;
+  int ndevices = wb_robot_get_number_of_devices();
+  for (i = 0; i < ndevices; i++) {
+    WbDeviceTag dtag = wb_robot_get_device_by_index(i);
+    const char *dname = wb_device_get_name(dtag);
+    WbNodeType dtype = wb_device_get_node_type(dtag);
+    if (dtype == WB_NODE_DISTANCE_SENSOR && strlen(dname) == 3 && dname[0] == 'g' && dname[1] == 's') {
+      int id = dname[2] - '0';
+      if (id >= 0 && id < GROUND_SENSORS_NUMBER) {
+        ground_sensors[id] = wb_robot_get_device(ground_sensors_names[id]);
+        wb_distance_sensor_enable(ground_sensors[id], get_time_step());
+      }
+    }
+  }
+
+  // get a handler to the motors and set target position to infinity (speed control).
+  left_motor = wb_robot_get_device("left wheel motor");
+  right_motor = wb_robot_get_device("right wheel motor");
+  wb_motor_set_position(left_motor, INFINITY);
+  wb_motor_set_position(right_motor, INFINITY);
+  wb_motor_set_velocity(left_motor, 0.0);
+  wb_motor_set_velocity(right_motor, 0.0);
+
+  step();
+}
+
+static void reset_actuator_values() {
+  int i;
+  for (i = 0; i < 2; i++)
+    speeds[i] = 0.0;
+  for (i = 0; i < LEDS_NUMBER; i++)
+    leds_values[i] = false;
+}
+
+static void get_sensor_input() {
+  int i;
+  for (i = 0; i < DISTANCE_SENSORS_NUMBER; i++) {
+    distance_sensors_values[i] = wb_distance_sensor_get_value(distance_sensors[i]);
+
+    // scale the data in order to have a value between 0.0 and 1.0
+    // 1.0 representing something to avoid, 0.0 representing nothing to avoid
+    distance_sensors_values[i] /= 4096;
+  }
+
+  for (i = 0; i < GROUND_SENSORS_NUMBER; i++) {
+    if (ground_sensors[i])
+      ground_sensors_values[i] = wb_distance_sensor_get_value(ground_sensors[i]);
+  }
+}
+
+static bool cliff_detected() {
+  int i;
+  for (i = 0; i < GROUND_SENSORS_NUMBER; i++) {
+    if (!ground_sensors[i])
+      return false;
+    if (ground_sensors_values[i] < 500.0)
+      return true;
+  }
+  return false;
+}
+
+static void set_actuators() {
+  int i;
+  for (i = 0; i < LEDS_NUMBER; i++)
+    wb_led_set(leds[i], leds_values[i]);
+  wb_motor_set_velocity(left_motor, speeds[LEFT]);
+  wb_motor_set_velocity(right_motor, speeds[RIGHT]);
+}
+
+static void blink_leds() {
+  static int counter = 0;
+  counter++;
+  leds_values[(counter / 10) % LEDS_NUMBER] = true;
+}
+
+static void run_braitenberg() {
+  int i, j;
+  for (i = 0; i < 2; i++) {
+    speeds[i] = 0.0;
+    for (j = 0; j < DISTANCE_SENSORS_NUMBER; j++)
+      speeds[i] += distance_sensors_values[j] * weights[j][i];
+
+    speeds[i] = offsets[i] + speeds[i] * MAX_SPEED;
+    if (speeds[i] > MAX_SPEED)
+      speeds[i] = MAX_SPEED;
+    else if (speeds[i] < -MAX_SPEED)
+      speeds[i] = -MAX_SPEED;
+  }
+  //empurra a caixa com distancia maior que zero
+  for(i=0; i < DISTANCE_SENSORS_NUMBER; i++){
+    if(distance_sensors_values[i]>0.1){
+      is_pushing_box = true;
+      initial_box_distance = distance_sensors_values[i];
+      break;
+    }
+  }
+    
+}
+
+static void go_backwards() {
+  wb_motor_set_velocity(left_motor, -MAX_SPEED);
+  wb_motor_set_velocity(right_motor, -MAX_SPEED);
+  passive_wait(0.2);
+}
+
+static void turn_left() {
+  wb_motor_set_velocity(left_motor, -MAX_SPEED);
+  wb_motor_set_velocity(right_motor, MAX_SPEED);
+  passive_wait(0.2);
+}
+
+int main(int argc, char **argv) {
+wb_robot_init();
+printf("Default controller of the e-puck robot started aaaaaaaaa...\n");
+
+init_devices();
+
+bool found_box = false;
+
+reset_actuator_values();
+WbNodeRef robotNode = wb_supervisor_node_get_from_def("CAIXA");
+const double *position_ini = wb_supervisor_node_get_position(robotNode);
+printf("Posição do robô fora: (%g, %g, %g)\n", position_ini[0], position_ini[1], position_ini[2]);
+
+double epsilon = 0.000500;
+double n1 = 2.17292e-311;
+double n2=8.69169e-311;
+char s1[12]; 
+char s2[12];
+bool controle=0;    
+while (true) {
+  get_sensor_input();
+  WbNodeRef boxtNode = wb_supervisor_node_get_from_def("CAIXA");
+  double *position_running = wb_supervisor_node_get_position(boxtNode);
+  //printf("Posição do robô dentro: (%g, %g, %g)\n", position_running[0], position_running[1], position_running[2]);
+  sprintf(s1, "%f", position_ini[0]);
+  sprintf(s2, "%f", position_ini[0]); 
+  int r1 = strcmp(s1, "0.000000");
+  int r2 = strcmp(s2, "0.000000");
+ 
+  if  ((r1==0|| r2==0) &&(controle==0)){
+     run_braitenberg();
+  }
+ else if  ((fabs(position_ini[0] - position_running[0]) < epsilon)&&(controle==0)){
+    printf("%d %g %g \n ",r1,position_running[0], position_ini[0]);
+     run_braitenberg();
+
+  }else{
+  
+  printf("aqui");
+    controle=1;
+    blink_leds();
+    //printf("bateu \n");
+    speeds[LEFT] = 0;
+    speeds[RIGHT] = 0;
+    
+     
+  }
+  
+  /*if (position_running[0] == position_ini[0]){
+    run_braitenberg();
+  }
+  else {
+   
+   
+    }*/
+  set_actuators();
+  step();
+}
+
+return EXIT_SUCCESS;
+}
+
